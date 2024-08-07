c0e31e96236346c0a8ec25bceaabb855700608ab
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-08-06 22:29:56 -0500

    Change default font

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const DEFAULT_THEME = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{svelte,ts,js,html}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', ...DEFAULT_THEME.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
