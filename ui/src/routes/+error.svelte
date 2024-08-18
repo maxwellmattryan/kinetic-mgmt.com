278b694ec1ff936f55f798666904d7df528a8689
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-08-17 23:02:45 -0500

    Create mobile-friendly error page

--- ui/src/routes/+error.svelte
+++ ui/src/routes/+error.svelte
@@ -7,9 +7,9 @@
 </svelte:head>
 
 <error-page>
-	<div class="h-[92vh] flex flex-col items-center justify-center">
+	<div class="h-[92vh] px-12 md:px-48 flex flex-col items-center justify-center">
 		<h1>Page Not Found</h1>
-		<p class="mt-8 text-zinc-500">
+		<p class="mt-8 text-zinc-500 text-center">
 			The page you requested couldn't be found - this could be due to a spelling error in the URL or
 			a removed page.
 		</p>
