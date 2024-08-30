a639c2856a72ee9c01132d0aa1d05d85f61576a9
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-08-30 11:51:04 -0500

    Add Tailwind font config for redundancy

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const DEFAULT_THEME = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{svelte,ts,js,html}'],
 	theme: {
-		extend: {},
+		extend: {
+			/**
+			 * Info about customizing the default font:
+			 * https://tailwindcss.com/docs/font-family#customizing-the-default-font
+			 */
+			fontFamily: {
+				sans: ['Montserrat', ...DEFAULT_THEME.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
